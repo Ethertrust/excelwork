bfc814e22dca78937fc0c88672a377d9b70cf9a3
Ethertrust <egoshvedo@mail.ru>
2016-10-12 14:33:26 +0500

    dfhg

--- ExcelPPC.py
+++ ExcelPPC.py
@@ -1,7 +1,25 @@
 import xlsxwriter
 import xlrd
 import re
-from itertools import count
+
+def step(row, number, parseStep1):
+
+    if pattern.search(row[0]):
+        writeToXLSX(row, number)
+        number += 1
+    else:
+        parseStep1(row, number)
+
+def parseStep1(row, number):
+     if row[8] == "Стаж ППС":
+         writeToXLSX(row, number)
+         number += 1
+
+def writeToXLSX(row, rownumber):
+    x = 0
+    for column in row:
+        worksheet.write(rownumber, x, column)
+        x += 1
 
 workbook = xlsxwriter.Workbook('excelsheets/result.xlsx')
 worksheet = workbook.add_worksheet()
@@ -14,23 +32,11 @@ print(vals)
 print(vals[1][0])
 pattern = re.compile('Кафедра*', re.IGNORECASE)
 number = 0
+
 for row in vals:
-    step(row, number, writeToXLSX, DOSMTE)
+    print(row[0])
+    step(row, number, parseStep1)
+workbook.close()
 
-
-
-def step(row, number, writexlsx, DOSMTE):
-    if pattern.search(row[0]):
-        writexlsx(row, number)
-        number += 1
-    else:
-        DOSMTE(row)
-
-def writeToXLSX(row, rownumber):
-    for column in row:
-        worksheet.write(rownumber, count(0), column)
-
-def DOSMTE(row):
-     if row[8] == "Стаж ППС":
 
 
